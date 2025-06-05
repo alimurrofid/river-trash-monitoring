3ba911188dc7eb4535b4ad0e2acb84458a95afd5
alimurrofid <alimurrofid77@gmail.com>
2025-06-05 19:07:26 +0700

    refactor:hailo postprocess

--- raspberrypi_hailoai/postprocess/compile_postprocess.sh
+++ raspberrypi_hailoai/postprocess/compile_postprocess.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-set -e
-
-# pastikan environment hailo aktif
-if [ -z "$CPATH" ]; then
-  echo "ERROR: Please run 'source /home/rivermonitor/Public/hailo-rpi5-examples/setup_env.sh' first!"
-  exit 1
-fi
+# Script ini untuk compile postprocess
 
 mkdir -p build
 cd build
 
 cmake ..
 make
-
-echo "Build finished. Shared object is at build/libriver_trash_post.so"
